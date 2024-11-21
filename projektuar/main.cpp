21bd98fa986d8b0be06136cea76975f64ac48e63
MATCZERNECKI <mc310501@student.polsl.pl>
2024-11-21 12:49:23 +0100

    siusiak hahahahaha

--- projektuar/main.cpp
+++ projektuar/main.cpp
@@ -22,6 +22,8 @@ protected:
 	//Nazwy pisze na szybko, do sprawdzenia
 	ARX * m_ARX = nullptr;
 	PID * m_PID = nullptr;
+	double m_zadane;
+	double m_zmierzone;
 public:
 	//Musze sprawdzic jak sie wyklucza konstruktor domyslny
 	void wykonajKrok()
@@ -29,34 +31,45 @@ public:
 		//Sprawdzenie czy arx i pid sa ustawione
 		// do dopisania w czwartek
 	}
-	Symulacja() = delete[]; // wykluczylem konstruktor domyslny
-	Symulacja(ARX* arx, PID * pid)
+	Symulacja() = delete; // wykluczylem konstruktor domyslny
+	Symulacja(ARX* arx, PID * pid, double zadane)
 	{
 		setARX(arx);
 		setPID(pid);
+		setZadane(zadane);
 	}
 	void setARX(ARX * arx)
 	{
 		if(arx != nullptr)
 		{
-			m_arx = arx;
+			m_ARX = arx;
 		}
 		else
 		{
-			throw invalid_argument("Nie podales regulatora!!!");
+			throw invalid_argument("Nie podales modelu!!!");
 		}
 	}
 	void setPID(PID * pid)
 		{
 			if(pid != nullptr)
 			{
-				m_pid = pid;
+				m_PID = pid;
 			}
 			else
 			{
-				throw invalid_argument("Nie podales ukladu czegos tam dopiszemy!!!");
+				throw invalid_argument("Nie podales regulatora!!!");
 			}
 		}
+	void setZadane(double zadane)
+	{
+		m_zadane = zadane;
+	}
+	double krok()
+	{
+		double sygnal = m_PID->oblicz(m_zadane, m_zmierzone);
+
+		//dokonczyc!!!!!!!!!!!!!!
+	}
 };
 class ARX
 {
@@ -131,36 +144,103 @@ public:
 class PID
 {
 protected:
-	double kp;
-	double ki;
-	double kd;
-	double suma;
-	double pop_blad;
+	double m_kp;
+	double m_ki;
+	double m_kd;
+	double m_suma;
+	double m_pop_blad;
 
 public:
 	PID(double p, double i, double d)
-		:kp(p), ki(i), kd(d), suma(0.0), pop_blad(0.0) {}
+		:m_kp(p), m_ki(i), m_kd(d), m_suma(0.0), m_pop_blad(0.0) {}
 
 	double oblicz(double cel, double zmierzone)
 	{
 		double blad = cel - zmierzone;
-		double P = kp * blad;
+		double P = m_kp * blad;
 
-		suma += blad;
+		m_suma += blad;
 		double I;
-		if (ki == 0.0)
+		if (m_ki == 0.0)
 		{
 			I = 0.0;
 		}
 		else {
-			I = suma / ki;
+			I = m_suma / m_ki;
 		}
 
-		double D = kd * (blad - pop_blad);
+		double D = m_kd * (blad - m_pop_blad);
 
 		return P + I + D;
 	}
 
+};
+
+enum TypSygnalu {skok, sinusoida, prostokatny};
+
+class WartoscZadana {
+protected:
+	TypSygnalu m_typsygnalu;
+	double m_amplituda;
+	double m_okres;
+	double m_cykl;
+	int m_aktywacja;
+	int m_czas;
+public:
+	WartoscZadana() : m_typsygnalu(skok), m_amplituda(1.0), m_okres(1.0), m_cykl(0.5), m_aktywacja(0), m_czas(0) {}
+	void ustawskok(double amplituda, int aktywacja)
+	{
+		m_typsygnalu = skok;
+		m_amplituda = amplituda;
+		m_aktywacja = aktywacja;
+	}
+	void ustawsinusoide(double amplituda, double okres)
+	{
+		m_typsygnalu = sinusoida;
+		m_amplituda = amplituda;
+		m_okres = okres;
+	}
+	void ustawprostokatny(double amplituda, double okres, double cykl)
+	{
+		m_typsygnalu = prostokatny;
+		m_amplituda = amplituda;
+		m_okres = okres;
+		m_cykl = cykl;
+	}
+	double generuj()
+	{
+		double wartosc = 0.0;
+
+		switch (m_typsygnalu) {
+		case skok:
+			if (m_czas >= m_aktywacja)
+			{
+				wartosc = m_amplituda;
+			}
+			else {
+				wartosc = 0.0;
+			}
+			break;
+		case sinusoida:
+			wartosc = m_amplituda * sin(2 * 3.14 * (m_czas % static_cast<int>(m_okres)) / m_okres);
+			break;
+		case prostokatny:
+			if ((m_czas % static_cast<int>(m_okres)) < (m_cykl * m_okres))
+			{
+				wartosc = m_amplituda;
+			}
+			else {
+				m_amplituda = 0.0;
+			}
+			break;
+		}
+		m_czas++;
+		return wartosc;
+	}
+	void reset()
+	{
+		m_czas = 0;
+	}
 };
 
 int main()
